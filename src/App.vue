b36703fbe3c12a6191c606f5c17b0a30171d1687
thelenaza <kaxtie07@gmail.com>
2024-05-28 12:36:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-link to = "/">Home</router-link> |
+    <router-link to = "/about">About</router-link>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h3>Home Page</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
